d8ba25a8d8d39ae2887575d008caa7fa8c463569
Juancol9 <146022086+Juancol9@users.noreply.github.com>
2023-10-09 15:55:05 -0500

    Update Ejercicio 05.js

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -1,145 +1,80 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*5️⃣ EJERCICIO 05 5️⃣*/
 
-function obtenerMayor(x, y) {
-   // "x" e "y" son números enteros.
-   // Retornar el número más grande.
-   // Si son iguales, retornar cualquiera de los dos.
+function esPositivo(num) {
+   // La función recibe un entero. Devuelve como resultado un string que indica si el número
+   // es positivo o negativo.
+   // Si el número es positivo ---> "Es positivo".
+   // Si el número es negativo ---> "Es negativo".
+   // Si el número es 0, devuelve false.
    // Tu código:
-   if (x>y)return x
-   else return y
+ if(num>0)return "Es positivo"
+ else if (num<0) return"Es negativo"
+ else return false
+
 }
 
-function mayoriaDeEdad(edad) {
-   // Determinar si la persona puede ingresar al evento según su edad.
-   // Si tiene 18 años ó más debe retornar el string: "Allowed".
-   // Caso contrario: "Not allowed".
+function agregarSimboloExclamacion(str) {
+   // Agrega un símbolo de exclamación al final del string "str" y retórnalo
+   // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
-   if (edad>17)return"Allowed"
-   return "Not allowed"
+   return str + "!"
 }
 
-function conection(status) {
-   // El argumento "status" representa el estado de conexión de un usuario.
-   // Si el estado es igual a 1, el usuario está "Online".
-   // Si el estado es igual a 2, el usuario está "Away".
-   // De lo contrario, presumimos que el usuario está "Offline".
-   // Retornar el estado de conexión del usuario.
+function combinarNombres(nombre, apellido) {
+   // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
+   // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
    // Tu código:
-if (status === 1) return "Online"
-else if (status===2) return "Away"
-else return "Offline"
+return nombre+ " "+ apellido
 }
 
-function saludo(idioma) {
-   // Retornar un saludo en tres diferentes lenguajes:
-   // Si "idioma" es "aleman", devuelve "Guten Tag!".
-   // Si "idioma" es "mandarin", devuelve "Ni Hao!".
-   // Si "idioma" es "ingles", devuelve "Hello!".
-   // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
+function obtenerSaludo(nombre) {
+   // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
+   // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
-if (idioma==="aleman") return "Guten Tag!";
-else if (idioma==="mandarin") return "Ni Hao!";
-else if (idioma==="ingles") return  "Hello!";
-else return "Hola!";
+   return "Hola" + nombre + "!"
+
 }
 
-function colors(color) {
-   // La función recibe un color. Retornar el string correspondiente:
-   // En caso que el color recibido sea "blue"   --> "This is blue".
-   // En caso que el color recibido sea "red"    --> "This is red".
-   // En caso que el color recibido sea "green"  --> "This is green".
-   // En caso que el color recibido sea "orange" --> "This is orange".
-   // Si no es ninguno de esos colores           --> "Color not found".
-   // IMPORTANTE: utilizar el statement SWITCH.
+function obtenerAreaRectangulo(alto, ancho) {
+   // Retornar el área de un rectángulo teniendo su altura y ancho.
    // Tu código:
-   if (color==="blue") return "This is blue";
-   else if (color=== "red") return "This is red";
-   else if(color==="green")return "This is green";
-   else if (color==="orange")return "This is orange";
-   return "Color not found"
+   return alto*ancho
+   
 }
 
-function esDiezOCinco(num) {
-   // Retornar true si "num" es 10 o 5.
-   // De lo contrario, retornar false.
+function retornarPerimetro(lado) {
+   // La función recibe como argumento la medida de un lado de un cuadrado.
+   // Debes retornar su perímetro.
    // Tu código:
-   if (num = 10) return true
-   else if(num = 5) return true
-   return false
-}
-function estaEnRango(num) {
-   // Retornar true si "num" es menor que 50 y mayor que 20.
-   // De lo contrario, retornar false.
-   // Tu código:
-if (num<50)
-   if (num>20) return true
-   return false
-}
-function esEntero(num) {
-   // Retornar true si "num" es un entero, ya sea positivo, negativo o cero.
-   // Ejemplo: 0.8   ---> false
-   // Ejemplo: 1     ---> true
-   // Ejemplo: (-10) ---> true
-   // De lo contrario, retorna false.
-   // Tu código:
-   if (num % 1 == 0)return true
-   return false
-}
-function fizzBuzz(num) {
-   // Si "num" es divisible entre 3, retorna "fizz".
-   // Si "num" es divisible entre 5, retorna "buzz".
-   // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
-   // De lo contrario, retorna false.
-   // Tu código:
-   if(num%(5*3)===0) return "fizzbuzz"
-   else if (num%3===0) return"fizz"
-   else if (num%5===0) return"fazz"
-   return false
+   return 4*lado
+   
 }
 
-function operadoresLogicos(num1, num2, num3) {
-   // La función recibe tres números distintos.
-   // Si num1 es mayor a num2 y a num3, y además es positivo, retornar ---> "Numero 1 es mayor y positivo".
-   // Si alguno de los tres números es negativo, retornar ---> "Hay negativos".
-   // Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
-   // Si todos los argumentos son cero, retornar ---> "Error".
-   // Si no se cumple ninguna de las condiciones anteriores, retornar false.
+function areaDelTriangulo(base, altura) {
+   // Calcula el área de un triángulo y retorna el resultado.
    // Tu código:
-   if (num1 > num2 && num1 > num3 && num1 > 0) return "Numero 1 es mayor y positivo";
-   if (num1 < 0 || num2 < 0 || num3 < 0) return "Hay negativos";
-   if (num3>num1 & num3>2) return ++ num3
-   else (num1=0 && num2 = 0 && num3 = 0  ) return "error";
+   return (base*altura)/2
 }
 
-
-function esPrimo(num) {
-   // Retornar true si "num" es primo.
-   // De lo contrario retorna false.
-   // [Pista 1]: un número primo sólo es divisible por sí mismo y por 1.
-   // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
-   // [Nota]: los números negativos, 0 y 1 NO son números primos.
+function deEuroAdolar(euro) {
+   // Supongamos que 1 euro equivale a 1.20 dólares.
+   // Debes calcular el valor recibido como argumento pasándolo a dolares.
    // Tu código:
-}
+   return euro*1.15
+   }
 
-function esVerdadero(valor) {
-   // Si "valor" es verdadero retornar "Soy verdadero".
-   // Caso contrario, retornar "Soy falso".
+function esVocal(letra) {
+   // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
+   // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
+   // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
-}
+   letra=letra.toLowerCase()
 
-function tieneTresDigitos(num) {
-   // Si el número recibido tiene tres dígitos retornar true.
-   // Caso contrario, retornar false.
-   // Tu código:
-}
+   if (letra==="a"||letra=="e"||letra=="i"||letra=="o"|| letra=="u") return"Es vocal"
+   else return "Dato incorrecto"
 
-function doWhile(num) {
-   // Implementar una función que aumente el valor recibido en 5 hasta un límite de 8 veces.
-   // Retornar el valor final.
-   // Utilizar el bucle Do-While.
-   // Tu código:
-}
+   }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
